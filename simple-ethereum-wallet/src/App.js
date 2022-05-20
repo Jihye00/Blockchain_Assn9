d1a4b54e605249937656c6be18146d4b0d59f2fe
Jihye Seo <seojh0813@postech.ac.kr>
2022-05-20 14:59:16 +0900

    DONE!

--- simple-ethereum-wallet/src/App.js
+++ simple-ethereum-wallet/src/App.js
@@ -3,15 +3,18 @@ import Web3 from 'web3';
 import Box from '@material-ui/core/Box';
 import Select from 'react-select';
 
-const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+const rpcURL = "http://localhost:8545"
+const web3 = new Web3(new Web3.providers.HttpProvider(rpcURL));
+console.log(web3.version.network)
 
 const GetAccountList = ({accountList, setAccountList, total, setTotal}) => {
-  web3.currentProvider.enable()
+  // web3.currentProvider.enable()
 
   async function getList() {
     var sum = 0;
     var accList = []
     const list = await web3.eth.getAccounts();
+    console.log(list)
     for (var acc of list) {
       var balance = Number(await web3.utils.fromWei(await web3.eth.getBalance(acc), "ether"))
       accList.push({ label: acc, value: balance })
@@ -57,7 +60,8 @@ const SendEther = ({accountList}) => {
         <input type="text" style={{ border: '1px solid skyblue' }} onChange={(e) => setPassword(e.target.value)}></input>
         <button onClick={() => {
           console.log(fromAddress.label, amount, toAddress, password)
-          web3.eth.sendTransaction({from:fromAddress.label, to: toAddress, value: web3.utils.toWei(amount)}, password).then(console.log)
+          // web3.eth.personal.unlockAccount(fromAddress.label)
+          web3.eth.personal.unlockAccount(fromAddress.label, password, 100).then(web3.eth.sendTransaction({from:fromAddress.label, to: toAddress, value: web3.utils.toWei(amount)}, password).then(console.log))
         }}>Send</button>
       </Box>
     </div>
